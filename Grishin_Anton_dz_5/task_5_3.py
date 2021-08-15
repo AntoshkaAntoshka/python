67908e773de614603661f9221c50c0553180a9f6
Anton <antongrishin1994@mail.ru>
2021-08-16 01:57:28 +0300

    Задание 5.3 - исправление!!!

--- Grishin_Anton_dz_5/task_5_3.py
+++ Grishin_Anton_dz_5/task_5_3.py
@@ -1,11 +1,20 @@
-import itertools
+from itertools import zip_longest
 
-tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена']
+tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей','Дмитрий', 'Борис', 'Елена']
 klasses = ['9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А']
 
-gen_tuple = (itertools.zip_longest(tutors, klasses, fillvalue='None')) # обрачиваем в генератор
-# собираем поочередно первые элементы списков и получаем список кортежей
-print(type(gen_tuple)) # <class 'itertools.zip_longest'> - работает как генератор, но класс вот такой...
+
+def finish_tuple():
+    if len(tutors) < len(klasses):                   # Если длина списка tutors меньше кол-ва классов, то используем zip
+        for name, klas in zip(tutors, klasses):      # Если длина списка tutors больше кол-ва классов, то zip_longest
+            yield name, klas                         
+    else:
+        for name, klas in zip_longest(tutors, klasses):
+            yield name, klas
+
+
+gen_tuple = finish_tuple()
+print(type(gen_tuple)) # <class 'generator'>
 print(next(gen_tuple)) # ('Иван', '9А')
 print(next(gen_tuple)) # ('Анастасия', '7В')
 print(next(gen_tuple)) # ('Петр', '9Б')
@@ -13,5 +22,4 @@ print(next(gen_tuple)) # ('Сергей', '9В')
 print(next(gen_tuple)) # ('Дмитрий', '8Б')
 print(next(gen_tuple)) # ('Борис', '10А')
 print(next(gen_tuple)) # ('Елена', '10Б')
-print(next(gen_tuple)) # ('None', '9А')
 print(next(gen_tuple)) # StopIteration
