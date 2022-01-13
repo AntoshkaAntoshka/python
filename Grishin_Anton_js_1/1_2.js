cdcb8b536580e27d0271fb9f2eae0f4ddf658e17
Anton <antongrishin1994@mail.ru>
2022-01-13 15:05:17 +0300

    Задание 1.2

---
+++ Grishin_Anton_js_1/1_2.js
@@ -0,0 +1,6 @@
+/*Объявить две переменные: admin и name. Записать в name строку "Василий"; Скопировать значение из name в admin. Вывести admin (должно вывестись «Василий»).*/
+
+var admin = null
+var name = "Василий"
+var admin = name
+alert(admin) // Василий
