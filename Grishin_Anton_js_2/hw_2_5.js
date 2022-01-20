473ad6b75319b1b3f5453b676d10397aaec2723a
Anton <antongrishin1994@mail.ru>
2022-01-20 18:16:56 +0300

    2.5

---
+++ Grishin_Anton_js_2/hw_2_5.js
@@ -0,0 +1,24 @@
+/* Реализовать четыре основные арифметические операции в виде функций с двумя параметрами. Обязательно использовать оператор return. */
+
+function sum(x, y) {
+    return x + y
+}
+
+function sub(x, y) {
+    return x - y
+}
+
+function mul(x, y) {
+    return x * y
+}
+
+function div(x, y) {
+    return x / y
+}
+
+var a = 4
+var b = 2
+sum(a, b) // 6
+sub(a, b) // 2
+mul(a, b) // 8
+div(a, b) // 2
